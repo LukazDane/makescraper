4bf95c113c371e7330ca2fde4c665f754c2d9c86
Luke <deandrevidal@aol.com>
2020-06-24 17:30:22 -0700

    initial commit

--- scrape.go
+++ scrape.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gocolly/colly"
 )
@@ -12,19 +13,44 @@ func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
-
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-	})
-
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong:", err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Visited", r.Request.URL)
+	})
+
+	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	// 	e.Request.Visit(e.Attr("href"))
+	// })
+
+	c.OnHTML("tr td:nth-of-type(1)", func(e *colly.HTMLElement) {
+		fmt.Println("First column of a table row:", e.Text)
+	})
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("body > div.o_content > div > div > div > a", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	})
+
+	c.OnXML("//h1", func(e *colly.XMLElement) {
+		fmt.Println(e.Text)
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
 	})
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("https://www.homestuck.com/log/story")
+
 }
