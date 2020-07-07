11de6859a3eec772e6e8597f15db9933fe9eeca0
Luke <deandrevidal@aol.com>
2020-07-06 20:53:21 -0700

    finished reqs

--- scrape.go
+++ scrape.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
+type pageName struct {
+	ListedAs string `json:"listedas"`
+	CodedAs  string `json:"codedas"`
+}
+
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
+
+	pages := []pageName{}
+
 	// Instantiate default collector
 	c := colly.NewCollector()
 
@@ -30,16 +41,20 @@ func main() {
 	// 	e.Request.Visit(e.Attr("href"))
 	// })
 
-	c.OnHTML("tr td:nth-of-type(1)", func(e *colly.HTMLElement) {
-		fmt.Println("First column of a table row:", e.Text)
-	})
+	// c.OnHTML("body > div.o_content > div > div > div > br", func(e *colly.HTMLElement) {
+	// 	fmt.Println("PAge and Date: ", e.Text)
+	// })
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("body > div.o_content > div > div > div > a", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-
+	c.OnHTML("body > div.o_content > div > div > div > a:nth-child(2n+2)", func(e *colly.HTMLElement) {
+		page := pageName{}
+		page.ListedAs = e.Text
+		p := e.Attr("href")
+		page.CodedAs = strings.TrimPrefix(p, "/story/")
 		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		pages = append(pages, page)
+		fmt.Printf("Page Title: %q -> %s\n", e.Text, page.ListedAs)
+		fmt.Printf("Page Code: %s\n", page.CodedAs)
 	})
 
 	c.OnXML("//h1", func(e *colly.XMLElement) {
@@ -52,5 +67,17 @@ func main() {
 
 	// Start scraping on https://hackerspaces.org
 	c.Visit("https://www.homestuck.com/log/story")
+	serializeToJSON(pages)
 
+}
+func writeFile(file []byte) {
+	if err := ioutil.WriteFile("output.json", file, 0644); err != nil {
+		log.Fatalf("Unable to write file! %v", err)
+	}
+}
+func serializeToJSON(n []pageName) {
+	fmt.Println("Serializing Pages to JSON...")
+	serialized, _ := json.Marshal(n)
+	writeFile(serialized)
+	fmt.Println("Successfully serialized the Homestuck to 'output.json'")
 }
